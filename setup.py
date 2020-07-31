de54e3b2aec4fbc18b3f109453ce2fc3d70741ca
Totzke, Patrick <patricktotzke@gmail.com>
2020-07-31 10:58:54 +0100

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 import setuptools
-import glob
-import os.path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -15,7 +13,7 @@ setuptools.setup(
     description="A check50 extension for java",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/pazz/check50_java",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -26,6 +24,5 @@ setuptools.setup(
     install_requires=[
         'check50',
     ],
-    #zip_safe=False,
     package_data={'check50_java': ['lib/*.jar']},
 )
