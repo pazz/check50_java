5d37df9255f2f32c98aae0da22d4dc840f394b53
Totzke, Patrick <patricktotzke@gmail.com>
2020-09-05 16:15:03 +0100

    work around circular imports

--- check50_java/__init__.py
+++ check50_java/__init__.py
@@ -1,9 +1,8 @@
 # Copyright (C) 2020  Patrick Totzke <patricktotzke@gmail.com>
 # This file is released under the GNU GPL, version 3 or a later revision.
 
-from . checks import is_application_class
-from . compile import compile, run
-
-#######
 JAVAC = "javac"
 JAVA = "java"
+
+from . compile import compile, run
+import checks
